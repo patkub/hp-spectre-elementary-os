7e493c6157bb4a47527f9d23aedafd5f5a4ba550
Patrick Kubiak <pk9948@rit.edu>
2020-01-02 20:02:56 -0500

    Create patkub-elementary-os-setup.sh
    start working on a setup script

---
+++ patkub-elementary-os-setup.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# enable wifi
+apt-get install linux-oem-osp1 linux-firmware
+
+# install google chrome
+wget -O /tmp/linux_signing_key.pub https://dl.google.com/linux/linux_signing_key.pub 
+apt-key add /tmp/linux_signing_key.pub
+apt-get update
+apt-get install google-chrome-stable
+rm /tmp/linux_signing_key.pub
+
+# install apps
+apt-get install \
+	audacity \
+	blender \
+	firefox \
+	gimp \
+	kazam \
+	krita \
+	mpv \
+	qbittorrent \
+	snapd \
+	steam \
+	textstudio
+
+# install snaps
+snap install slack --classic
+
+# vscode
+snap install code --classic
+
+snap install discord
+
+# nodejs
+snap install node --classic
+
+# notetaking
+snap install xournalpp
