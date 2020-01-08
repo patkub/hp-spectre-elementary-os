615ef639d8edf7dd9318835b163bb77f7d5e180a
Patrick Kubiak <pk9948@rit.edu>
2020-01-08 02:18:12 -0500

    remove unused color variables

--- setup.sh
+++ setup.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# colors
-red=`tput setaf 1`
-green=`tput setaf 2`
-yellow=`tput setaf 3`
-blue=`tput setaf 4`
-magenta=`tput setaf 5`
-cyan=`tput setaf 6`
-reset=`tput sgr0`
-
 # ask for sudo
 if [ $EUID != 0 ]; then
   sudo "$0" "$@"
